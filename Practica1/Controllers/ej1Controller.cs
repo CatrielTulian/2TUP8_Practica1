53f51da64e38a8fff376c21cce43b2aa65329bd0
CatrielTulian <109996753+CatrielTulian@users.noreply.github.com>
2024-08-07 22:35:10 -0300

    Ejercicio 1 y 2

---
+++ Practica1/Controllers/ej1Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ej1Controller : ControllerBase
+    {
+        [HttpGet("Suma")]
+
+        public int Get(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+        
+    }
+}
---
+++ Practica1/Controllers/ej2Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ej2Controller : ControllerBase
+    {
+        
+        [HttpGet("NombreCiudad")]
+        public string Get(string nombre, string ciudad)
+        {
+            return $"Hola {nombre}, Bienvenido a {ciudad}";
+        }
+    }
+}
